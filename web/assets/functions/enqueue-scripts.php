1d580fc5f9e8676d5a556ce75609e6665d697f11
Jim <jim@mediakanjers.nl>
2026-06-16 11:27:30 +0200

    mobiele weergave ontwikkeling

--- web/assets/functions/enqueue-scripts.php
+++ web/assets/functions/enqueue-scripts.php
@@ -1,15 +1,12 @@
 <?php
 
 function mk_enqueue_fonts() {
-    // Termina via Adobe Fonts (Typekit)
     wp_enqueue_style(
         'mk-font-termina',
         'https://use.typekit.net/num1qrx.css',
         array(),
         null
     );
-
-    // Inter via Google Fonts
     wp_enqueue_style(
         'mk-font-inter',
         'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
@@ -20,21 +17,7 @@ function mk_enqueue_fonts() {
 add_action( 'wp_enqueue_scripts', 'mk_enqueue_fonts' );
 
 function mk_enqueue_scripts() {
-    wp_enqueue_script(
-        'mk-projecten-slider',
-        get_stylesheet_directory_uri() . '/dist/js/projecten-slider.js',
-        ['swiper-js'],
-        null,
-        true
-    );
-    wp_enqueue_script(
-        'mk-projecten-gallery',
-        get_stylesheet_directory_uri() . '/dist/js/projecten-gallery.js',
-        ['swiper-js', 'fancybox-js'],
-        null,
-        true
-    );
+    // Swiper laden zodat scripts.min.js (loop-slider, projecten-slider, gallery) Swiper kan gebruiken
+    wp_enqueue_script( 'swiper-js' );
 }
 add_action( 'wp_enqueue_scripts', 'mk_enqueue_scripts' );
-
-?>
---
+++ web/template-parts/blocks/mk-loop/render.php
@@ -0,0 +1,46 @@
+<?php
+$post_type = get_field('loop_post_type') ?: 'deuren-kozijnen';
+$aantal    = (int) ( get_field('loop_aantal') ?: 8 );
+$kolommen  = (int) ( get_field('loop_kolommen') ?: 4 );
+$img_uri   = get_stylesheet_directory_uri() . '/dist/images';
+
+$posts = get_posts([
+    'post_type'      => $post_type,
+    'posts_per_page' => $aantal,
+    'post_status'    => 'publish',
+    'orderby'        => 'menu_order',
+    'order'          => 'ASC',
+]);
+
+if ( ! $posts ) return;
+?>
+
+<div class="mk-loop mk-loop--type-<?php echo esc_attr( $post_type ); ?>">
+    <div class="swiper mk-loop__swiper">
+        <div class="swiper-wrapper mk-loop__grid" data-grid="<?php echo esc_attr( $kolommen ); ?>">
+            <?php foreach ( $posts as $post ) :
+                $id      = $post->ID;
+                $link    = get_permalink( $id );
+                $title   = get_the_title( $id );
+                $img_id  = get_post_thumbnail_id( $id );
+                $img_url = $img_id ? wp_get_attachment_image_url( $img_id, 'large' ) : '';
+                $img_alt = $img_id ? get_post_meta( $img_id, '_wp_attachment_image_alt', true ) : $title;
+            ?>
+            <div class="swiper-slide">
+                <a href="<?php echo esc_url( $link ); ?>" class="mk-loop__card">
+                    <?php if ( $img_url ) : ?>
+                    <div class="mk-loop__image">
+                        <img src="<?php echo esc_url( $img_url ); ?>"
+                             alt="<?php echo esc_attr( $img_alt ); ?>"
+                             loading="lazy">
+                    </div>
+                    <?php endif; ?>
+                    <div class="mk-loop__content">
+                        <h3 class="mk-loop__title"><?php echo esc_html( $title ); ?></h3>
+                    </div>
+                </a>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
